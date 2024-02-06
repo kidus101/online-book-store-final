8fffa7c47483d87f1858233a740634b77830b1d7
kidus <kidusabebe101@gmail.com>
2024-02-06 20:36:45 +0300

    Removing components to reduce complexity

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
-import Services from "./components/Services/Services.jsx";
-import Banner from "./components/Banner/Banner.jsx";
-import AppStore from "./components/AppStore/AppStore.jsx";
-import Testimonial from "./components/Testimonial/Testimonial.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import AOS from "aos";
 import "aos/dist/aos.css";
